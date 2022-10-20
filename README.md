407c261b994fc61581156e3ec81414c5a69cfe02
huizi <wanghui66@lenovo.com>
2022-10-20 16:21:44 +0800

    插件

---
+++ README.md
@@ -0,0 +1,7 @@
+### 使用方式:
+
+1. 安装 pnpm install 
+
+2.  使用pnpm install -g  进行全局安装  或者  pnpm link 进行链接
+
+3. 执行rename-cli  
---
+++ bin/index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const {
+	program
+} = require("commander")
+const inquirer = require('inquirer');
+const chalk = require("chalk")
+const camelcase = require("camelcase")
+inquirer.prompt([{
+	type: 'input',
+	message: '请输入要更改的key,以[:|-|_|#]分割',
+	name: 'name'
+}]).then((answer) => {
+	if (['', null, undefined].includes(answer.name)) {
+		console.log(chalk.red("请输入正确的key!")) 
+		return
+	}
+	const splitArray = answer.name.split(/[:_\-\#]/)
+	console.log(chalk.blue(camelcase(splitArray,{pascalCase:true}))) 
+})
+
